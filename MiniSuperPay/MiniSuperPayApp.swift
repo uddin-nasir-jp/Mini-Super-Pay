acd724c6b9e005fee9755f9368c5b5ca913dbc47
Nasir Uddin <uddin.nasir.jp@gmail.com>
2025-11-28 19:01:22 +0900

    Initial Commit

---
+++ MiniSuperPay/MiniSuperPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniSuperPayApp.swift
+//  MiniSuperPay
+//
+//  Created by Nasir Uddin on 28/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MiniSuperPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
